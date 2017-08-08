9229cb6da2f69a169eafedce5bfbc18774d34cf6
Лехап <@->
2017-08-08 21:09:46 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.michomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.отладкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.звонокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.неБеспокоитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.дзыыньToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 48);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(823, 464);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.michomToolStripMenuItem,
+            this.звонокToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(847, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // michomToolStripMenuItem
+            // 
+            this.michomToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.неБеспокоитьToolStripMenuItem,
+            this.отладкаToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.выходToolStripMenuItem});
+            this.michomToolStripMenuItem.Name = "michomToolStripMenuItem";
+            this.michomToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.michomToolStripMenuItem.Text = "Michom";
+            // 
+            // отладкаToolStripMenuItem
+            // 
+            this.отладкаToolStripMenuItem.Name = "отладкаToolStripMenuItem";
+            this.отладкаToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.отладкаToolStripMenuItem.Text = "Отладка";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(153, 6);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // звонокToolStripMenuItem
+            // 
+            this.звонокToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.дзыыньToolStripMenuItem});
+            this.звонокToolStripMenuItem.Name = "звонокToolStripMenuItem";
+            this.звонокToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.звонокToolStripMenuItem.Text = "Звонок";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(463, 48);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(250, 250);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            this.webBrowser1.Visible = false;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Michom console";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // неБеспокоитьToolStripMenuItem
+            // 
+            this.неБеспокоитьToolStripMenuItem.Name = "неБеспокоитьToolStripMenuItem";
+            this.неБеспокоитьToolStripMenuItem.Size = new System.Drawing.Size(156, 22);
+            this.неБеспокоитьToolStripMenuItem.Text = "Не беспокоить";
+            this.неБеспокоитьToolStripMenuItem.Click += new System.EventHandler(this.неБеспокоитьToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 525);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Отправить команду";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(180, 526);
+            this.textBox1.MaxLength = 50;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(197, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(396, 525);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "На";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(670, 525);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 21);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // дзыыньToolStripMenuItem
+            // 
+            this.дзыыньToolStripMenuItem.Name = "дзыыньToolStripMenuItem";
+            this.дзыыньToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.дзыыньToolStripMenuItem.Text = "Дзыынь";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(451, 525);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 565);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Michom Console";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem michomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem отладкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem звонокToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripMenuItem неБеспокоитьToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem дзыыньToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        bool sleep = false;
+        string lag;
+
+        string call = "192.168.1.0";
+
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.RunWorkerAsync();
+            comboBox1.Items.Add(call);
+        }
+
+        public void Setlog(string data, string url, bool priem)
+        {
+            string date = DateTime.Now.ToString();
+            richTextBox1.Text += date + " ";
+            richTextBox1.Text += data;
+            if (priem)
+            {
+                richTextBox1.Text += " <- ";
+            }
+            else
+            {
+                richTextBox1.Text += " -> ";
+            }
+            richTextBox1.Text += url;
+            richTextBox1.Text += Environment.NewLine;
+        }
+
+        public void CmdCont(string data, string url)
+        {
+            webBrowser1.Navigate(new Uri("http://" + url + "/" + data));
+            Setlog(data, url, false);
+        }
+
+        public void Refresh(string url)
+        {
+            webBrowser1.Navigate(new Uri("http://" + url + "/refresh"));
+            Setlog("Обновление", url, false);
+            if (webBrowser1.DocumentText == "Call") {
+                if (sleep) {
+                    Setlog("Звонок в дверь", url, true);
+                    notifyIcon1.ShowBalloonTip(2000, "Звонок в дверь", "Кто-то звонит в дверь", ToolTipIcon.None);
+                }
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Label lab = new Label();
+            //Form1 frm = new Form1();
+            //lab.Text = "400000000000000000000000000000000000000000000000000000000000000000000000";
+            //this.label1.Text = "84;";
+            //while (true)
+            //{
+            //    Console.WriteLine("testtttttttttttttttttttttttttttttttttttttt");
+            //}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Setlog("Starting","OK",false);
+            //notifyIcon1.ShowBalloonTip(1000, "Привет", "Привет, ты запустил Michom", ToolTipIcon.None);
+            timer1.Start();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            Setlog(webBrowser1.DocumentText, " ", true);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+           // Refresh("");
+            timer1.Start();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, EventArgs e)
+        {
+            ShowBalloonTip();
+            
+        }
+        private void ShowBalloonTip()
+        {
+            Container bpcomponents = new Container();
+            ContextMenu contextMenu1 = new ContextMenu();
+
+            MenuItem runMenu = new MenuItem();       
+            runMenu.Index = 1;
+            runMenu.Text = "Michom";
+            runMenu.Click += new EventHandler(runMenu_Click);
+
+            MenuItem breakMenu = new MenuItem();
+            breakMenu.Index = 2;
+            breakMenu.Text = "-------------";            
+
+            MenuItem sleep = new MenuItem();                                 
+            sleep.Index = 3;
+            sleep.Text = "Не беспокоить";
+            sleep.Click += new EventHandler(sleep_Click);
+
+            MenuItem exitMenu = new MenuItem();
+            exitMenu.Index = 4;
+            exitMenu.Text = "E&xit";
+            exitMenu.Click += new EventHandler(exitMenu_Click);
+            /*
+            MenuItem exitMenu = new MenuItem();
+            exitMenu.Index = 5;
+            exitMenu.Text = "E&xit";
+            exitMenu.Click += new EventHandler(exitMenu_Click);
+
+            MenuItem exitMenu = new MenuItem();
+            exitMenu.Index = 6;
+            exitMenu.Text = "E&xit";
+            exitMenu.Click += new EventHandler(exitMenu_Click);
+            */
+            // Initialize contextMenu1
+            contextMenu1.MenuItems.AddRange(
+                        new System.Windows.Forms.MenuItem[] { runMenu, breakMenu, sleep, exitMenu });
+            notifyIcon1.ContextMenu = contextMenu1;
+            notifyIcon1.Visible = true;
+        }
+
+        void exitMenu_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        void runMenu_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+        void sleep_Click(object sender, EventArgs e)
+        {
+            if (sleep)
+            {
+                notifyIcon1.ShowBalloonTip(500, "Сон", "Уведомления поступают", ToolTipIcon.Info);
+                sleep = false;
+            }
+            else
+            {
+                notifyIcon1.ShowBalloonTip(200, "Сон", "Уведомления не поступают", ToolTipIcon.Info);
+                sleep = true;
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void неБеспокоитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sleep)
+            {
+                notifyIcon1.ShowBalloonTip(500, "Сон", "Уведомления поступают", ToolTipIcon.Info);
+                sleep = false;
+            }
+            else
+            {
+                notifyIcon1.ShowBalloonTip(50, "Сон", "Уведомления не поступают", ToolTipIcon.Info);
+                sleep = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string comand = textBox1.Text;
+            string adress = comboBox1.Text;
+            CmdCont(comand, adress);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string line;
+            if (File.Exists(Directory.GetCurrentDirectory() + "\\log.txt"))
+            {            
+            StreamReader sr = new StreamReader(Directory.GetCurrentDirectory() + "\\log.txt");
+                line = sr.ReadLine();
+
+                //Continue to read until you reach end of file
+                while (line != null)
+                {
+                    //write the lie to console window
+                    //Console.WriteLine(line);
+                    lag += line + Environment.NewLine;
+                    //Read the next line
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+                StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + "\\log.txt");
+
+                string[] words = richTextBox1.Text.Split(new char[] { '\n' });
+
+                foreach (string s in words)
+                {
+                    sw.WriteLine(s);
+                }
+                
+
+                // sw.WriteLine("From the StreamWriter class");
+                //Close the file
+                sw.Close();
+            }
+            else
+            {
+                StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + "\\log.txt");
+
+                string[] words = richTextBox1.Text.Split(new char[] { '\n' });
+
+                foreach (string s in words)
+                {
+                    sw.WriteLine(s);
+                }
+
+                // sw.WriteLine("From the StreamWriter class");
+                //Close the file
+                sw.Close();
+            }
+         }
+    }
+}
