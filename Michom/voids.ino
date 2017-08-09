90332c6d33cc24b31bb774b7e0f5230a153b9f46
Microfcorp <a.mostowenko2012@yandex.ru>
2017-08-09 15:42:41 +0300

    Codes for NodeMcu

---
+++ Michom/voids.ino
@@ -0,0 +1,16 @@
+void SetData(String data, int url){
+
+}
+
+String GetData(String data){
+  String dat = data;
+  return dat;
+}
+
+String ret(String data){
+String dat = data+"OK";
+return dat;
+}
+
+
+
