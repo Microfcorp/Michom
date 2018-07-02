652d6c404d9d0c3deb180bdea593505d80f603e5
Alesha <a.mostowenko2012@yandex.ru>
2018-07-02 07:26:45 +0300

    Add Scripts Light Module

---
+++ Michom/Miclight/voids.ino
@@ -0,0 +1,13 @@
+String parsejson(String type, String data){
+  String temp = "";
+  temp += "{";
+  temp += "\"ip\": \"" + WiFi.localIP().toString() + "\",";
+  temp += "\"type\":";
+  temp += "\"" + type + "\",";
+  if(type == "get_light_status"){    
+  temp += "\"data\": {";
+  temp += "\"status\": \"" + data + "\" } } \r\n";
+  }
+  return temp; 
+}
+
