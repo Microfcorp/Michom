5d46fbfa22cdf5c2bd6d1cfb81a15e35f8873264
Alesha <a.mostowenko2012@yandex.ru>
2018-07-09 16:22:45 +0300

    Add files via upload

---
+++ Michom/MichomSborInfo/voids.ino
@@ -0,0 +1,17 @@
+String parsejson(String type, String data){
+  String temp = "";
+  temp += "{";
+  temp += "\"ip\": \"" + WiFi.localIP().toString() + "\",";
+  temp += "\"type\":";
+  temp += "\"" + type + "\",";
+  if(type == "msinfoo"){    
+  temp += "\"data\": {";
+  temp += "\"davlen\": \"" + String(bmp.readPressure()/133.3) + "\",";
+  temp += "\"temperbmp\": \"" + String(bmp.readTemperature()) + "\",";
+  temp += "\"visot\": \"" + String(bmp.readAltitude()) + "\",";
+  temp += "\"temper\": \"" + String(dht.readTemperature()) + "\",";
+  temp += "\"humm\": \"" + String(dht.readHumidity()) + "\" } } \r\n";
+  }
+  return temp; 
+}
+
