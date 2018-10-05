6f0a2a2bfa06fc233dcb34eedd2588a215c99e39
Alesha <a.mostowenko2012@yandex.ru>
2018-10-05 20:15:34 +0300

    Add files via upload

--- Gateway/michome/getpost.php
+++ Gateway/michome/getpost.php
@@ -29,7 +29,7 @@ $obj = json_decode($getjson);
 $ip = $obj->{'ip'};
 $type = $obj->{'type'};
 if($type == "msinfoo"){	
-	$temperdht = $obj->{'data'}->{'temper'};
+	//$temperdht = $obj->{'data'}->{'temper'};
 	$temp = $obj->{'data'}->{'temperbmp'};
 	
 	//$temp = ($temperdht + $temperbmp) / 2;
@@ -37,10 +37,10 @@ if($type == "msinfoo"){
 	$davlen = $obj->{'data'}->{'davlen'};
 	$visot = $obj->{'data'}->{'visot'};
 	
-	if($temperdht != "nan"){
+	//if($temperdht != "nan"){
 	$guery = "INSERT INTO `michom`(`id`, `ip`, `type`, `data`, `temp`, `humm`, `dawlen`, `visota`, `date`) VALUES ('$id', '$ip', 'msinfoo','null','$temp','$humm','$davlen','$visot','$date')"; 
 	$result = mysqli_query($link, $guery);
-	}
+	//}
 }
 elseif($type == "termometr"){	
 	$temper = $obj->{'data'}->{'temper'};
