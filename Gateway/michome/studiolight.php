74370fe1ac83708a5464f8ce82391bf2e881029e
Alesha <a.mostowenko2012@yandex.ru>
2018-10-11 20:27:05 +0300

    Add strobo

--- Gateway/michome/studiolight.php
+++ Gateway/michome/studiolight.php
@@ -83,6 +83,22 @@ while (new Date() < ms){}
 	}
 	sleep(500);
    }
+   
+   function Strobo(p, s, d) {
+	   
+	var size = s;
+
+	var host = '<?echo $_SERVER['HTTP_HOST'];?>';
+	
+	if(host != "192.168.1.42"){
+	//console.log('http://<?echo $_SERVER['HTTP_HOST'];?>/michome/api/setcmd.php?device='+ '192.168.1.34' +'&cmd='+ 'setlight?p='+p+'%26s='+size);
+	postAjax('http://<?echo $_SERVER['HTTP_HOST'];?>/michome/api/setcmd.php?device='+ '192.168.1.34' +'&cmd='+ 'strobo?p='+p+'%26s='+size+'%26t='+d, "", function(){});
+	}
+	else{
+		//console.log('http://192.168.1.34/setlight?p='+p+'&s='+size);
+		postAjax('http://192.168.1.34/strobo?p='+p+'&s='+size+'&t='+d, "", function(){});
+	}	
+   }
   </script>
 </head>
 
@@ -90,12 +106,15 @@ while (new Date() < ms){}
 	<?php include_once("/var/www/html/site/verh.php"); ?>
 	<H1 style="text-align: center; color:red;">Управление Michome. План комнат. Управление освещением</H1>
 	
-	<div style='float: left; background-color: #D4EB66; width: 420px; height: 250px'>
+	<div style='float: left; background-color: #D4EB66; width: 420px; height: 310px'>
 	
 	<p>Свет 1: <input type="range" min="0" max="255" oninput="sizePic(0, this.value)" value="0"><input type="number" min="0" max="255" oninput="sizePic(0, this.value)" value="0"><input type="button" onclick="sizePic(0, 255)" value="На всю"><input type="button" onclick="sizePic(0, 0)" value="На 0"></p>
 	<p>Свет 2: <input type="range" min="0" max="255" oninput="sizePic(1, this.value)" value="0"><input type="number" min="0" max="255" oninput="sizePic(1, this.value)" value="0"><input type="button" onclick="sizePic(1, 255)" value="На всю"><input type="button" onclick="sizePic(1, 0)" value="На 0"></p>
 	<p>Свет 3: <input type="range" min="0" max="255" oninput="sizePic(2, this.value)" value="0"><input type="number" min="0" max="255" oninput="sizePic(2, this.value)" value="0"><input type="button" onclick="sizePic(2, 255)" value="На всю"><input type="button" onclick="sizePic(2, 0)" value="На 0"></p>
-	
+	<br />
+	<p>Свет 1 Стробо: <input type="number" min="0" max="100" id="sb1" value="3"><input type="number" min="0" max="500" id="st1" value="30"><input type="button" onclick="Strobo(0, document.getElementById('sb1').value, document.getElementById('st1').value)" value="Старт"></p>
+	<p>Свет 2 Стробо: <input type="number" min="0" max="100" id="sb2" value="3"><input type="number" min="0" max="500" id="st2" value="30"><input type="button" onclick="Strobo(1, document.getElementById('sb2').value, document.getElementById('st2').value)" value="Старт"></p>
+	<p>Свет 3 Стробо: <input type="number" min="0" max="100" id="sb3" value="3"><input type="number" min="0" max="500" id="st3" value="30"><input type="button" onclick="Strobo(2, document.getElementById('sb3').value, document.getElementById('st3').value)" value="Старт"></p>
 	</div>
 </body>
 </html>
