ab1780ba7858026bfe845313036d4b9b8c8aac32
Alesha <a.mostowenko2012@yandex.ru>
2019-07-13 09:33:07 +0300

    Add files via upload

--- PythonModule/LightingPacages.py
+++ PythonModule/LightingPacages.py
@@ -2,34 +2,51 @@ import urllib.request
 import sys
 import json
 import time
+import telnetlib
 
 path = ""
+type = "-t";
 
 print ("Запуск...")
 
-if len (sys.argv) > 1:
+if len (sys.argv) > 2:
+    type = sys.argv[2]
+elif len (sys.argv) > 1:
 	path = sys.argv[1]
 	print ("Успешно запущенно")
 else:
 	exit("Ошибка ключей!")
-	
 
-def Send(deistv, pin, light, delay, delay1):
-	if gateway == "192.168.1.42":
-		print("Посылаю команду напрямую")
-		print('http://192.168.1.34/'+deistv+'?p='+pin+"&q="+light+"&d="+delay)
-		my_web = urllib.request.urlopen('http://192.168.1.34/'+deistv+'?p='+pin+"&q="+light+"&d="+delay+"&s="+delay1)
-		print("Ответ: " + my_web.read().decode('UTF-8'))
-	else:
-		print("Посылаю команду через шлюз")
-		my_web = urllib.request.urlopen('http://91.202.27.167/michome/api/setcmd.php?device=192.168.1.34&cmd='+deistv+'?p='+pin+"%26q="+light+"%26d="+delay)
-		print("Ответ: " + my_web.read().decode('UTF-8'))
+if type == "-t":
+    print ("Работа через telnet") 
+    tn = telnetlib.Telnet("192.168.1.34")
+else:
+    print ("Работа через HTTP")
+
+
+def Send(deistv = "", pin  = "", light  = "", delay  = "", delay1  = ""):
+    if gateway == "192.168.1.42":        
+        if type == "-t":
+            print("Посылаю команду напрямую через Telnet")
+            key = bytes(deistv+";"+pin+";"+light+";"+delay+";"+delay1, encoding="utf_8")
+            print(key)
+            tn.write(key)
+            time.sleep(0.1)
+        else:
+            print("Посылаю команду напрямую через HTTP")
+            #print('http://192.168.1.34/'+deistv+'?p='+pin+"&q="+light+"&d="+delay)
+            my_web = urllib.request.urlopen('http://192.168.1.34/'+deistv+'?p='+pin+"&q="+light+"&d="+delay+"&s="+delay1)
+            print("Ответ: " + my_web.read().decode('UTF-8'))
+    else:
+        print("Посылаю команду через шлюз")
+        my_web = urllib.request.urlopen('http://91.202.27.167/michome/api/setcmd.php?device=192.168.1.34&cmd='+deistv+'?p='+pin+"%26q="+light+"%26d="+delay)
+        print("Ответ: " + my_web.read().decode('UTF-8'))
 	
 def SendKODI(file):
 	if gateway == "192.168.1.42":
 		print("Посылаю команду напрямую")
 		my_web = urllib.request.urlopen('http://192.168.1.42:8080/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22id%22:%221%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22'+file+'%22}}}')
-		print('http://192.168.1.42:8080/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22id%22:%221%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22'+file+'%22}}}');
+		#print('http://192.168.1.42:8080/jsonrpc?request={%22jsonrpc%22:%222.0%22,%22id%22:%221%22,%22method%22:%22Player.Open%22,%22params%22:{%22item%22:{%22file%22:%22'+file+'%22}}}');
 		if json.loads(my_web.read().decode('UTF-8'))['result'] == 'OK':
 			print("Успешно")
 		else:
